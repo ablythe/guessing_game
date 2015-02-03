ed6fe1f65e854e9652d558b7e9dc9058ffc5f06b
Alexander Blythe <Alexander.blythe@gmail.com>
2015-02-03 12:25:19 -0500

    Added player choice options

--- play_game.rb
+++ play_game.rb
@@ -2,11 +2,26 @@ require './game'
 require './player'
 
 g = Game.new
-p = DumbAi.new
+puts "Choose mode"
+puts "1) Human"
+puts "2) DumbAi"
+puts "3) SmartAi"
+choice = gets.chomp.to_i
+
+player_class = if choice == 1
+  Player
+elsif choice  == 2
+  DumbAi
+else
+  SmartAi
+end
+
+p = player_class.new
 until g.over?
   guess = p.get_guess
   puts "You guessed: #{guess}"
   response =g.check_guess guess
+  p.update_guess response
   puts response
 end
 
--- player.rb
+++ player.rb
@@ -3,10 +3,34 @@ class Player
     print "What is your guess? "
     gets.chomp.to_i
   end
+
+  def update_guess a 
+  end
 end
 
-class DumbAi
+class DumbAi < Player
   def get_guess
     rand 1.. 100
+  end
+end
+
+class SmartAi < Player
+  def initialize
+    @low = 0
+    @high = 100
+    @last_guess = nil
+  end
+
+  def get_guess
+    new_guess = (@low + @high) /2
+    @last_guess = new_guess
+  end  
+
+  def update_guess response
+    if response.include? "high"
+      @high = @last_guess
+    elsif response.include? "low"
+      @low = @last_guess
+    end
   end
 end
